60502f3ef915cbca077cdb186d6d77a12781fd17
Vishwas Kumar Verma <vishwasverma23@gmail.com>
2023-12-28 17:32:19 +0530

    some upgradation

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
 function fun1(){
     url=("myresume.pdf")
-    window.open(url, "blank")
+    window.open(url, "_blank")
 }
 
 function pro1(){
 
     let url=(" https://vishwas23a.github.io/Calculator/")
-    window.open(url, "blank")
+    window.open(url, "_blank")
 }
 function pro2(){
     let url=("https://the-roadmap.vercel.app/")
-    window.open(url,"blank");
+    window.open(url,"_blank");
 }
 
 function github(){
     url=("https://github.com/vishwas23a")
-    window.open(url,"blank")
+    window.open(url,"_blank")
 }
 function hacker(){
     url=("https://www.hackerrank.com/profile/vishswasverma23")
-    window.open(url,"blank")
+    window.open(url,"_blank")
 }
 function leet(){
     url=("https://leetcode.com/vishwas2313/")
-    window.open(url,"blank")
+    window.open(url,"_blank")
 }
 function mail(){
     let url=("mailto:vishwasverma23@gmail.com")
-    window.open(url,"blank")
+    window.open(url,"_blank")
 }
 function certificate(){
     let url=("next.html")
-    window.open(url, "blank")
+    window.open(url, "_blank")
 }
